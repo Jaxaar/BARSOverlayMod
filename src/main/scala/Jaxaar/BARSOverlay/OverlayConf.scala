5fab899030ec4b9463e7f021984be5cfcdb5ce90
Jaxaar <jax1664-cs@yahoo.com>
2024-04-15 19:07:49 -0400

    Streamlining getting value

--- src/main/scala/Jaxaar/BARSOverlay/OverlayConf.scala
+++ src/main/scala/Jaxaar/BARSOverlay/OverlayConf.scala
@@ -12,27 +12,30 @@ object OverlayConf {
 				fieldLength = 90,
 				getString = (p) => s"[${if(p.playerLoaded) p.player.getIntProperty("achievements.bedwars_level", 0) else 0}✫] ${p.getTrueName}",
 				specialFormat = true,
-				colorBreakpoints = List(0)
+				colorBreakpoints = List(0),
+				value = new SingleValue("achievements.bedwars_level", "Int")
 			),
 			new OverlayColumnValues(
 				title = "WLR",
 				fieldLength = 40,
 				getString = (p) => s"${if(p.playerLoaded) (p.player.getDoubleProperty("stats.Bedwars.wins_bedwars", 0) / p.player.getDoubleProperty("stats.Bedwars.losses_bedwars", 1) *100).round/100.0 else 0}",
 				colorBreakpoints =  List(1,2,5,7,10),
-			    getValue = (p) => if(p.playerLoaded) (p.player.getDoubleProperty("stats.Bedwars.wins_bedwars", 0) / p.player.getDoubleProperty("stats.Bedwars.losses_bedwars", 1) *100).round/100.0 else 0
+			    getValue = (p) => if(p.playerLoaded) (p.player.getDoubleProperty("stats.Bedwars.wins_bedwars", 0) / p.player.getDoubleProperty("stats.Bedwars.losses_bedwars", 1) *100).round/100.0 else 0,
+				value = new RatioValue("stats.Bedwars.wins_bedwars", "stats.Bedwars.wins_bedwars")
 			),
 			new OverlayColumnValues(
 				title = "FKDR",
 				fieldLength = 40,
 				getString = (p) => s"${if(p.playerLoaded) (p.player.getDoubleProperty("stats.Bedwars.final_kills_bedwars", 0) / p.player.getDoubleProperty("stats.Bedwars.final_deaths_bedwars", 1) *100).round/100.0 else 0}",
 				colorBreakpoints =  List(1,3,5,10,25),
-				getValue = (p) => if(p.playerLoaded) (p.player.getDoubleProperty("stats.Bedwars.final_kills_bedwars", 0) / p.player.getDoubleProperty("stats.Bedwars.final_deaths_bedwars", 1) *100).round/100.0 else 0
+				getValue = (p) => if(p.playerLoaded) (p.player.getDoubleProperty("stats.Bedwars.final_kills_bedwars", 0) / p.player.getDoubleProperty("stats.Bedwars.final_deaths_bedwars", 1) *100).round/100.0 else 0,
+				value = new RatioValue("stats.Bedwars.final_kills_bedwars", "stats.Bedwars.final_deaths_bedwars")
 			)
 		)
 	}
 }
 
-class OverlayColumnValues(val title: String, val fieldLength: Int, val getString: HypixelPlayerData => String, val specialFormat: Boolean = false, val colorBreakpoints: List[Double] = List(), val getValue: HypixelPlayerData => Double = (p) => 0){
+class OverlayColumnValues(val title: String, val fieldLength: Int, val getString: HypixelPlayerData => String, val specialFormat: Boolean = false, val colorBreakpoints: List[Double] = List(), val getValue: HypixelPlayerData => Double = (p) => 0, val value: SingleValue){
 	override def toString: String = s"${title} - ${fieldLength}"
 
 	def getFormattedString(player: HypixelPlayerData): String = {
@@ -59,10 +62,15 @@ class OverlayColumnValues(val title: String, val fieldLength: Int, val getString
 		val numGreater = breakpoints.count(_ >= d)
 		colors((breakpoints.length) - numGreater)
 	}
+
+	def getVal(player: HypixelPlayerData): Any = {
+		value.getValueMethod()(player)
+	}
+
 }
 
 class SingleValue(val stringPath: String, val typeStr: String){
-	def getValueMethod: HypixelPlayerData => Any = {
+	def getValueMethod(): HypixelPlayerData => Any = {
 		typeStr match {
 			case "String" => (player: HypixelPlayerData) => if(player.playerLoaded) player.player.getProperty(stringPath)
 			case "Int" => (player: HypixelPlayerData) => if(player.playerLoaded) player.player.getIntProperty(stringPath, 0)
